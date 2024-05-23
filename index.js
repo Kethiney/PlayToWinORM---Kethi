0be183d64f52d7035552f6855632b481143fdf7a
Kethiney <kethiney.brito@estudante.ifms.edu.br>
2024-05-23 15:38:03 -0400

    commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require ("dotenv").config();
-const conn = require("./db/conn");
+const conn = require("./db/conn")
 
-const Usuario =  require("./models/Usuario");
+const Usuario =  require("./models/Usuario")
 
-const express = require ("express");
+const express = require ("express")
+
+const exphbs = require ("express-handlebars")
+
   const app = express();
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
 
   app.use(
     express.urlencoded({
@@ -12,8 +18,23 @@ const express = require ("express");
     })
   )
 
+app.use(express.json())
+
+app.get("/", (req, res) =>{
+  res.render("home");
+})
+
+app.get("/usuarios", async (req, res) =>{
+
+     const usuarios = await Usuario.findAll({ raw: true});
+
+     res.render("usuarios", {usuarios});
+
+}
+)
+
   app.get("/usuarios/novo", (req,res) => {
-    res.sendFile(`${__dirname}/views/formUsuario.html`)
+    res.render("formUsuario")
   })
 
   app.post("/usuarios/novo" , async(req,res) => {
